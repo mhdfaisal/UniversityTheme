ae2ab070d818c00581c0e0c6f602e0c12650452b
Mohd <fmohd195@gmail.com>
2019-02-05 02:45:03 +0530

    App Finalized - Scrolling javascript added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
 $(document).ready(function(){
+
+    //Calculating windows innerWidth
+    var innerwidth = window.innerWidth;
+    $(window).resize(function(){
+        innerwidth = window.innerWidth;
+    });
+
+    
+
+    //Navigation color change effect on scrolling 
+
     let scrollPoint = $('.utility-nav').offset();
+    if(innerwidth >= 768){
+        $(window).scroll(function(){
 
-   $(window).scroll(function(){
-       
-       console.log( scrollPoint.top);
-       if(window.scrollY >= scrollPoint.top+100){
+        //console.log( scrollPoint.top);
+        if(window.scrollY >= scrollPoint.top+50){
 
-        $('.utility-nav').css({"background":"#0062CC"});
-        $('.primary-nav').css({"background":"#fff", "color":"#000 !important"});
-       }
-       else if(window.scrollY <= scrollPoint.top+100){
+        $('.utility-nav').css({"background":"#001933"});
+        $('.primary-nav').css({"background":"#0062CC"});
+        }
+        else if(window.scrollY <= scrollPoint.top+50){
 
             $('.utility-nav').css({"background":"transparent"});
-            $('.primary-nav').css({"background":"transparent", "color":"#fff"});
-       }
-   })
+            $('.primary-nav').css({"background":"transparent","padding":0});
+        }
+        });
+    }
+
+    // Explore Button Click
+    $("#explore-btn").click(function(e){
+        e.preventDefault();
+        var pos = $("#events").offset();
+        $('html, body').animate({scrollTop:pos.top}, 500);
+    })
 });
